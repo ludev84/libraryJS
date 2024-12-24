4a5b16d9fd175876b7fb7e55bf8815d17d6fb82c
Luis <58019574+ludev84@users.noreply.github.com>
2024-12-23 18:32:47 -0600

    Display list of books in a table

--- main.js
+++ main.js
@@ -1,14 +1,49 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function () {
-      return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`;
-    };
-  }
-  
-  const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
-  
-  console.log(theHobbit.info());
-  
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${
+      this.read ? "read" : "not read yet"
+    }`;
+  };
+}
+
+function addBookToLibrary(title, author, pages, read=false) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("Dune", "Frank Herbert", 350, false);
+addBookToLibrary("Flowers for Algernon", "Daniel Keyes", 250, true);
+addBookToLibrary("The Philosopher's Stone", "Colin Wilson", 325);
+
+function displayLibrary(myLibrary) {
+  const container = document.querySelector(".books");
+  myLibrary.map(
+    book => {
+      const tr = document.createElement("tr")
+
+      const thTitle = document.createElement("th");
+      thTitle.textContent = book.title;
+
+      const thAuthor = document.createElement("th");
+      thAuthor.textContent = book.author;
+
+      const thPages = document.createElement("th");
+      thPages.textContent = book.pages;
+
+      const thRead = document.createElement("th");
+      thRead.textContent = book.read ? "Yes" : "No";
+
+      tr.append(thTitle, thAuthor, thPages, thRead);
+      container.appendChild(tr)
+    }
+  )
+}
+
+displayLibrary(myLibrary);
